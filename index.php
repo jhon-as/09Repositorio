6014b221bc2fdd06a89c801aaa6d7a6212e69b0b
jhonastuchao <1004320181@unajma.edu.pe>
2020-07-18 12:56:11 -0500

    Cuadrado de nxn

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+    
+    function cuadrado( $num ){
+        for($i=1;$i<=$num;$i++){
+            for($j=1;$j<=$num;$j++){
+                echo " * ";
+            }
+            echo "<br>";
+        }
+    }
+
+?>
