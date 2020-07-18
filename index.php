eae11497cbdb3624bdb11736820262d3647f64b6
jhonastuchao <1004320181@unajma.edu.pe>
2020-07-18 13:14:11 -0500

    cuadrado solo con bordes resuelta

--- index.php
+++ index.php
@@ -3,7 +3,16 @@
     function cuadrado( $num ){
         for($i=1;$i<=$num;$i++){
             for($j=1;$j<=$num;$j++){
+               if($i == 1 || $i == $num){  
                 echo " * ";
+                }
+                if($i > 1 && $i!=$num){
+                    if($j == 1 || $j == $num){
+                        echo " * ";
+                    }else if($j!=1 || $j!=$num){
+                        echo "   ";
+                    }
+                }
             }
             echo "<br>";
         }
