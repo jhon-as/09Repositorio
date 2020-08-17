3b6dbc150bdb0ad7d8734eabbb9ab086e9bb791d
jhonastuchao <1004320181@unajma.edu.pe>
2020-08-17 16:50:33 -0500

    funcion suma

---
+++ suma.php
@@ -0,0 +1,15 @@
+<?php
+ 
+function suma($a, $b){
+    return $a + $b;
+}
+function resta($a, $b){
+    return $a - $b;
+}
+function multi($a, $b){
+    return $a * $b;
+}
+function divicion($a, $b){
+    return $a/$b;
+}
+?>
